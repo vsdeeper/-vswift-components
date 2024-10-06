36e3e3f33ab8f6f8a0e16f718ed679531ee1e80b
vsdeeper <vsdeeper@qq.com>
2024-10-06 15:24:12 +0800

    feat: improve s-select, s-cascader

--- src/components/vs-search/components/s-cascader/s-cascader.vue
+++ src/components/vs-search/components/s-cascader/s-cascader.vue
@@ -1,23 +1,38 @@
 <script setup lang="ts">
-import type { CascaderValue } from 'element-plus'
-import type { SCascaderProps } from '.'
+import type { CascaderOption, CascaderValue } from 'element-plus'
+import type { CascaderPropTypes, SCascaderProps } from '.'
 
-defineProps<{
+const _props = defineProps<{
   props?: SCascaderProps
 }>()
 
 const model = defineModel<CascaderValue>()
+const options = ref<CascaderOption[]>([])
+
+watch(
+  () => _props.props?.options,
+  async () => {
+    if (Array.isArray(_props.props?.options)) {
+      options.value = _props.props!.options
+    } else if (Object.prototype.toString.call(_props.props?.options) === '[object AsyncFunction]') {
+      options.value = await _props.props!.options!()
+    }
+  },
+  { once: true }
+)
 </script>
 
 <template>
   <el-cascader
     v-model="model"
-    v-bind="{
-      ...props,
-      clearable: props?.clearable ?? true,
-      filterable: props?.filterable ?? true,
-      props: { ...props?.props, value: props?.props?.value ?? 'id' }
-    }"
+    v-bind="
+      {
+        ...props,
+        clearable: props?.clearable ?? true,
+        filterable: props?.filterable ?? true,
+        props: { ...props?.props, value: props?.props?.value ?? 'id', options }
+      } as CascaderPropTypes
+    "
   >
   </el-cascader>
 </template>
--- src/components/vs-search/index.ts
+++ src/components/vs-search/index.ts
@@ -1,13 +1,14 @@
 import VsSearch from './vs-search.vue'
 
 export * from './components'
+
 export interface VsSearchOptionItem {
   // 唯一标识，与后端交互字段名称
   id: string
   // 组件类型
   type?: 'input' | 'select' | 'cascader' | 'date' | 'time' | 'tree-select'
   label?: string
-  // el组件的源属性设置
+  // el组件的源属性设置或其他自定义属性
   props?: Record<string, any>
 }
 export type VsSearchOptions = VsSearchOptionItem[]
